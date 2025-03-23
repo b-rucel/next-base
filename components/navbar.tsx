4e7bef7753e4f3470dbdb052236890d9298b5a90
bruce <549914-brucelim@users.noreply.gitlab.com>
2025-03-22 23:28:09 -0700

    updating github links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -29,7 +29,7 @@ export const NAVLINKS = [
   },
   {
     title: "Community",
-    href: "https://github.com/nisabmohd/Aria-Docs/discussions",
+    href: "https://github.com/b-rucel/next-base/discussions",
   },
 ];
 
@@ -62,7 +62,7 @@ export function Navbar() {
 
           <div className="flex items-center gap-2">
             <Link
-              href="https://github.com/nisabmohd/NexDocs"
+              href="https://github.com/b-rucel/next-base"
               className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all size-9 hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50"
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-[1.1rem] w-[1.1rem]">
